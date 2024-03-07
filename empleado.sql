37b287611533964ac1385b2e640cdc73b76fb06e
AMBARPAE03 <141931115+AMBARPAE03@users.noreply.github.com>
2024-03-07 10:22:33 -0500

    Add files via upload

---
+++ empleado.sql
@@ -0,0 +1,88 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 07-03-2024 a las 16:20:16
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `comercial_foods`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `empleado`
+--
+
+CREATE TABLE `empleado` (
+  `cod_empleado` int(11) NOT NULL,
+  `nombre1` varchar(25) NOT NULL,
+  `nombre2` varchar(25) NOT NULL,
+  `apellido1` varchar(25) NOT NULL,
+  `tipo_documento` enum('CC','CE','NIT','RUT') DEFAULT NULL,
+  `no_documento` varchar(25) NOT NULL,
+  `sexo` enum('masculino','femenino') DEFAULT NULL,
+  `direccion` varchar(25) NOT NULL,
+  `edad` int(11) NOT NULL CHECK (`edad` >= 18),
+  `telefono` varchar(25) NOT NULL,
+  `fecha_ingreso` date DEFAULT curdate(),
+  `estado_civil` enum('soltero','casado','divorciado','union libre','viudo') DEFAULT NULL,
+  `cargo_cod` int(11) NOT NULL,
+  `nivel_estudios` enum('primaria','bachillerato','tecnico','tecnologo','profecional','otro') DEFAULT NULL,
+  `eps` enum('sanitas','sura','capital salud','nueva eps','famisanar','aliansalud') DEFAULT NULL,
+  `pensiones` enum('colfondos','porvenir','skandia','proteccion') DEFAULT NULL,
+  `cesantias` enum('fna','porvenir','colfondos','proteccion') DEFAULT NULL,
+  `banco` enum('BBVA','davivienda','bancolombia','caja social','popular','las villas') DEFAULT NULL,
+  `activo` varchar(4) NOT NULL DEFAULT 'A',
+  `apellido2` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `empleado`
+--
+ALTER TABLE `empleado`
+  ADD PRIMARY KEY (`cod_empleado`),
+  ADD KEY `nombre1` (`nombre1`),
+  ADD KEY `cargo_cod` (`cargo_cod`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `empleado`
+--
+ALTER TABLE `empleado`
+  MODIFY `cod_empleado` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `empleado`
+--
+ALTER TABLE `empleado`
+  ADD CONSTRAINT `empleado_ibfk_1` FOREIGN KEY (`cargo_cod`) REFERENCES `cargo` (`cod_cargo`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
